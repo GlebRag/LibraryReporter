b9db975388253eb94165b755351c555362c109ce
Gleb <flareon.game@gmail.com>
2025-04-29 14:14:22 +0300

    Test Commit

--- LibraryReporter/Controllers/HomeController.cs
+++ LibraryReporter/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace LibraryReporter.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller // MainController
     {
         private readonly ILogger<HomeController> _logger;
 
